ffbaba1d1755444a9ba7fe03e52da91f4b6ff5aa
Aendarion <tipaho3abp@gmail.com>
2024-08-15 08:56:16 +0300

    #3 Update login page styles and tweak import error [RT Wallet Auth]

--- frontend/src/app/rt-wallet-auth/rt-wallet-auth.module.ts
+++ frontend/src/app/rt-wallet-auth/rt-wallet-auth.module.ts
@@ -1,20 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
 import {RtWalletAuthRootComponent} from './components/rt-wallet-auth-root/rt-wallet-auth-root.component';
 import {RtWalletAuthLoginComponent} from './components/rt-wallet-auth-login/rt-wallet-auth-login.component';
-import {RouterOutlet} from '@angular/router';
 import {RtWalletAuthRoutingModule} from './rt-wallet-auth-routing.module';
+import {RtWalletModule} from '../rt-wallet/rt-wallet.module';
 
 @NgModule({
-  declarations: [
-    RtWalletAuthRootComponent,
-    RtWalletAuthLoginComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    RtWalletAuthRoutingModule,
-  ],
+  declarations: [RtWalletAuthRootComponent, RtWalletAuthLoginComponent],
+  imports: [CommonModule, RouterOutlet, RtWalletAuthRoutingModule, MatCard, MatCardContent, MatButton, RtWalletModule],
 })
-export class RtWalletAuthModule {
-}
+export class RtWalletAuthModule {}
